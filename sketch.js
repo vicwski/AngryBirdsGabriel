ef5a461343b46c8e3ec4ba210c156b91e5b1a33f
vicwski <vic.gajewski@gmail.com>
2022-04-19 18:23:18 -0300

    sketch changes

--- sketch.js
+++ sketch.js
@@ -3,29 +3,32 @@ const World = Matter.World
 const Bodies = Matter.Bodies
 
 var engine, world
-var box1
+var ground
+var box1, box2, box3, box4, box5
+var log1, log2, log3, log4
+var pig1, pig2
+var bird
 
 function setup() {
   var canvas = createCanvas(1200, 400)
   engine = Engine.create()
   world = engine.world
 
-  ground = new Ground(600, height, 1200, 20)
+  ground = new Ground(width / 2, height - 10, width, 20)
 
   box1 = new Box(700, 320, 70, 70)
   box2 = new Box(920, 320, 70, 70)
-  pig1 = new Pig(810, 350)
-  log1 = new Log(810, 260, 300, PI / 2)
-
   box3 = new Box(700, 240, 70, 70)
   box4 = new Box(920, 240, 70, 70)
-  pig3 = new Pig(810, 220)
-
-  log3 = new Log(810, 180, 300, PI / 2)
-
   box5 = new Box(810, 160, 70, 70)
-  log4 = new Log(760, 120, 150, PI / 7)
-  log5 = new Log(870, 120, 150, -PI / 7)
+
+  pig1 = new Pig(810, 350)
+  pig2 = new Pig(810, 220)
+
+  log1 = new Log(810, 260, 300, PI / 2)
+  log2 = new Log(810, 180, 300, PI / 2)
+  log3 = new Log(760, 120, 150, PI / 7)
+  log4 = new Log(870, 120, 150, -PI / 7)
 
   bird = new Bird(100, 100)
 }
@@ -33,23 +36,22 @@ function setup() {
 function draw() {
   background(0)
   Engine.update(engine)
-  console.log(box2.body.position.x)
-  console.log(box2.body.position.y)
-  console.log(box2.body.angle)
+
+  ground.display()
+
   box1.display()
   box2.display()
-  ground.display()
-  pig1.display()
-  log1.display()
-
   box3.display()
   box4.display()
-  pig3.display()
-  log3.display()
-
   box5.display()
+
+  pig1.display()
+  pig2.display()
+
+  log1.display()
+  log2.display()
+  log3.display()
   log4.display()
-  log5.display()
 
   bird.display()
 }
