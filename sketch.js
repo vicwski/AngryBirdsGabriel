7dcd929cafaff26117ffd016025e654864025554
VicGA <vic.gajewski@gmail.com>
2022-05-19 17:10:57 -0300

    Aula C27

--- sketch.js
+++ sketch.js
@@ -1,13 +1,14 @@
 const Engine = Matter.Engine
 const World = Matter.World
 const Bodies = Matter.Bodies
+const Constraint = Matter.Constraint
 
 var engine, world
-var ground, backgroundImg
+var ground, plataform, backgroundImg
 var box1, box2, box3, box4, box5
-var log1, log2, log3, log4
+var log1, log2, log3, log4, log5
 var pig1, pig2
-var bird
+var bird, chain
 
 function preload() {
   backgroundImg = loadImage('sprites/bg.png')
@@ -19,6 +20,7 @@ function setup() {
   world = engine.world
 
   ground = new Ground(width / 2, height - 10, width, 20)
+  plataform = new Ground(150, height - 10, 300, 400)
 
   box1 = new Box(700, 320, 70, 70)
   box2 = new Box(920, 320, 70, 70)
@@ -33,9 +35,9 @@ function setup() {
   log2 = new Log(810, 180, 300, PI / 2)
   log3 = new Log(760, 120, 150, PI / 7)
   log4 = new Log(870, 120, 150, -PI / 7)
+  log5 = new Log(200, 120, 150, -PI / 7)
 
   bird = new Bird(100, 100)
-
 }
 
 function draw() {
@@ -43,6 +45,7 @@ function draw() {
   Engine.update(engine)
 
   ground.display()
+  plataform.display()
 
   box1.display()
   box2.display()
@@ -59,4 +62,5 @@ function draw() {
   log4.display()
 
   bird.display()
+  log5.display()
 }
